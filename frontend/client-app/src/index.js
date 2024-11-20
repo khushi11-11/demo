4b89375367ff9dcccf520c92b6462ba7a65a0c09
Khushi Verma <khushi7991012@gmail.com>
2024-11-20 22:21:53 +0530

    demo

--- frontend/client-app/src/index.js
+++ frontend/client-app/src/index.js
@@ -14,8 +14,8 @@ import Feedback from './Forms/Feedback';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Feedback />
-    {/* <Register /> */}
+    {/* <MainPage /> */}
+    <Register />
   </React.StrictMode>
 );
 
